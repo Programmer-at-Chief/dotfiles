a7c685617417434257785d0395c4dce82a01474e
Programmer-at-Chief <amansingh.chauhan.mat23@itbhu.ac.in>
2024-07-04 01:48:25 +0530

    nvim added

---
+++ nvim/lua/options.lua
@@ -0,0 +1,9 @@
+require "nvchad.options"
+
+-- add yours here!
+
+local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
+-- o.relativenumber = 'true'
+vim.wo.relativenumber = true
+vim.opt.mouse = "" 
